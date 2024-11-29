b1f9d02291b23907ba973f4325e36f50047d0fa6
Larvact <tobyg7794@hotmail.com>
2024-11-29 19:42:56 +0000

    Added solution to find the longest palindrome substring on a given string

---
+++ src/main/java/org/toby/personal/leetcode/medium/LongestPalindromeSubstring.java
@@ -0,0 +1,42 @@
+package org.toby.personal.leetcode.medium;
+
+import java.util.Objects;
+
+public class LongestPalindromeSubstring
+{
+
+    public static String getLongestPalindromeSubstring(final String string)
+    {
+        if(Objects.isNull(string)){return null;}
+
+        var longestPalindromeSubstring = "";
+        var stringCharacterArray = string.toCharArray();
+
+        for(char characterIndex = 0; characterIndex < string.length(); characterIndex++)
+        {
+            var localSubstring = String.valueOf(string.charAt(characterIndex));
+            var localSubstringIsPalindrome = true;
+            var palindromeSearchIncrementingCount = 1;
+
+            while (localSubstringIsPalindrome && characterIndex - palindromeSearchIncrementingCount > -1 && characterIndex + palindromeSearchIncrementingCount < stringCharacterArray.length - 1)
+            {
+                final var nextLhsChar = String.valueOf(stringCharacterArray[characterIndex - palindromeSearchIncrementingCount]);
+                final var nextRhsChar = String.valueOf(stringCharacterArray[characterIndex + palindromeSearchIncrementingCount]);
+                if(nextLhsChar.equals(nextRhsChar))
+                {
+                    localSubstring = nextLhsChar + localSubstring + nextRhsChar;
+                    if(localSubstring.length() > longestPalindromeSubstring.length())
+                    {
+                        longestPalindromeSubstring = localSubstring;
+                    }
+                    palindromeSearchIncrementingCount++;
+                }
+                else
+                {
+                    localSubstringIsPalindrome = false;
+                }
+            }
+        }
+        return longestPalindromeSubstring;
+    }
+}
---
+++ src/test/java/org/toby/personal/leetcode/medium/TestLongestPalindromeSubstring.java
@@ -0,0 +1,39 @@
+package org.toby.personal.leetcode.medium;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+
+class TestLongestPalindromeSubstring
+{
+    @Test
+    void nullString_getLongestPalindromeSubstring_returnNull()
+    {
+        Assertions.assertNull(LongestPalindromeSubstring.getLongestPalindromeSubstring(null));
+    }
+
+    private static Stream<Arguments> provideStringsAndSubstringResult()
+    {
+        return Stream.of(Arguments.of("", ""),
+                Arguments.of("a", "a"),
+                Arguments.of("ab", "b"),
+                Arguments.of("cabag", "aba"),
+                Arguments.of("gdsgbbbrfh", "bbb")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsAndSubstringResult")
+    void givenString_getLongestPalindromeSubstring_returnSubstring(final String string, final String expectedSubstring)
+    {
+        Assertions.assertEquals(expectedSubstring, LongestPalindromeSubstring.getLongestPalindromeSubstring(string));
+    }
+
+
+
+}
