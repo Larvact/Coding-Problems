654d814d27cf289a02c7d779bd83eb14cd92c8ce
Larvact <tobyg7794@hotmail.com>
2024-11-30 23:14:07 +0000

    Added base start/solution for the zigzag problem for one row

--- src/main/java/org/toby/personal/leetcode/medium/ZigzagConversion.java
+++ src/main/java/org/toby/personal/leetcode/medium/ZigzagConversion.java
@@ -1,6 +1,8 @@
 package org.toby.personal.leetcode.medium;
 
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ZigzagConversion
 {
@@ -10,11 +12,41 @@ public class ZigzagConversion
         {
             return "";
         }
+        //create rowNum arrays
+        final var resultArrays = new char[rowNum][string.length()];
+
+        //flag when insert index is 0
+        boolean isIndexFullInsert = true;
+        //track what array we are on
+        var insertionIndex = 0;
+        var insertionArrayIndex = 0;
+        //for letter in string
+        for(int index = 0; index < string.length(); index++)
+        {
+            final var character = string.charAt(index);
+            resultArrays[insertionArrayIndex][insertionIndex] = character;
+            insertionIndex += 1;
+        }
 
 
-
-
-
-        return null;
+        return Arrays.stream(resultArrays)
+                .map(String::valueOf)
+                .filter(arrayString -> !arrayString.isBlank())
+                .collect(Collectors.joining());
     }
 }
+
+/*
+            if(insertionArrayIndex % (rowNum - 1) == 0)
+            {
+                isIndexFullInsert = !isIndexFullInsert;
+            }
+            if(isIndexFullInsert)
+            {
+                insertionArrayIndex = insertionArrayIndex + 1;
+            }
+            else
+            {
+                insertionArrayIndex = insertionArrayIndex - 1;
+            }
+ */
--- src/test/java/org/toby/personal/leetcode/medium/TestZigzagConversion.java
+++ src/test/java/org/toby/personal/leetcode/medium/TestZigzagConversion.java
@@ -3,13 +3,16 @@ package org.toby.personal.leetcode.medium;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestZigzagConversion
 {
-
     @Test
     void zeroRows_convertToZigZag_emptyStringReturned()
     {
@@ -21,6 +24,23 @@ class TestZigzagConversion
     void blankString_convertToZigZag_emptyStringReturned(final String string)
     {
         assertEquals("", ZigzagConversion.convertToZigZag(string, 3));
+    }
+
+    private static Stream<Arguments> provideStringRowNumbersExpectedZigZagResult()
+    {
+        return Stream.of(
+                Arguments.of("PAYPALISHIRING", 1, "PAYPALISHIRING")
+                //Arguments.of("PAYPALISHIRING", 2, "PYAIHRNAPLSIIG"), //? Verify manually if others pass
+                //Arguments.of("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"),
+                //Arguments.of("PAYPALISHIRING", 4, "PINALSIGYAHRPI")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringRowNumbersExpectedZigZagResult")
+    void givenStringAndRowNumber_convertToZigZag_zigZagResultReturned(final String string, final int rowNumber, final String expectedZigZag)
+    {
+        assertEquals(expectedZigZag, ZigzagConversion.convertToZigZag(string, rowNumber));
     }
 
 
