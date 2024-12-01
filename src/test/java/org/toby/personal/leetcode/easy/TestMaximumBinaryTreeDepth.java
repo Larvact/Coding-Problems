e25842a89f0529ff26a390c674a904148d824ae9
Larvact <tobyg7794@hotmail.com>
2024-12-01 22:06:28 +0000

    added unit tst to verify the maximumbinarytreedepth for a bigger binary tree

--- src/test/java/org/toby/personal/leetcode/easy/TestMaximumBinaryTreeDepth.java
+++ src/test/java/org/toby/personal/leetcode/easy/TestMaximumBinaryTreeDepth.java
@@ -38,4 +38,15 @@ class TestMaximumBinaryTreeDepth
         Assertions.assertEquals(2, MaximumBinaryTreeDepth.maxDepth(root));
     }
 
+    @Test
+    void fullBinaryTree_maxDepth_returnCorrectDepth()
+    {
+        final var secondLhsChild = new TreeNode(111);
+        final var firstLhsChild = new TreeNode(6, secondLhsChild, null);
+        final var secondRhsChild = new TreeNode(12);
+        final var firstRhsChild = new TreeNode(6, null, secondRhsChild);
+        final var root = new TreeNode(1, firstLhsChild, firstRhsChild);
+
+        Assertions.assertEquals(3, MaximumBinaryTreeDepth.maxDepth(root));
+    }
 }
